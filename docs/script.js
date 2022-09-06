2bfc985408b7e3f625c7302ce8794c0d28f72aad
olidec <69003898+olidec@users.noreply.github.com>
2022-09-06 10:13:05 +0200

    added pixels

---
+++ docs/script.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById('c');
+  if (canvas.getContext) {
+    // Access the rendering context
+    var ctx = canvas.getContext('2d');
+    // ImageData object
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    function paint() {
+      // data represents the Uint8ClampedArray containing the data
+      // in the RGBA order [r0, g0, b0, a0, r1, g1, b1, a1, ..., rn, gn, bn, an]
+  
+      newData = [120,0,0,255,120,0,0,255,120,0,0,255,120,0,0,255]
+
+      imageData.data.set(newData);
+      ctx.putImageData(imageData, 0, 0);
+    }
+    paint();
+  }
