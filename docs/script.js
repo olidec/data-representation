1987f1db2877ed22ad1e2d49d7c761c09f6ecc9d
olidec <69003898+olidec@users.noreply.github.com>
2022-09-09 14:52:52 +0200

    checkerboard

--- docs/script.js
+++ docs/script.js
@@ -4,20 +4,34 @@ var canvas = document.getElementById('c');
     var ctx = canvas.getContext('2d');
     // ImageData object
     var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
+    
     function paint() {
       // data represents the Uint8ClampedArray containing the data
       // in the RGBA order [r0, g0, b0, a0, r1, g1, b1, a1, ..., rn, gn, bn, an]
+      let numPixels = imageData.data.length/4;
+
+      newData = [];
+      for (let i = 0; i < numPixels; i++) {
+        newData[4 * i + 3] = 255;
+      };
+
+      for (let i = 0; i < numPixels; i += 2) {
+        newData[4 * i] = 255
+      }
+      
+      for (let i = 1; i < numPixels; i += 2) {
+        newData[4 * i + 1] = 255
+      }
   
-      newData = [ 120,120,0,255,
-                  120,0,120,255,
-                  120,120,0,255,
-                  120,0,120,255,
-                  120,120,0,255,
-                  120,0,120,255,
-                  120,120,0,255,
-                  120,0,120,255,
-                  120,120,0,255]
+      // newData = [ 120,120,0,255,
+      //             120,0,120,255,
+      //             120,120,0,255,
+      //             120,0,120,255,
+      //             120,120,0,255,
+      //             120,0,120,255,
+      //             120,120,0,255,
+      //             120,0,120,255,
+      //             120,120,0,255];
                   
       imageData.data.set(newData);
       ctx.putImageData(imageData, 0, 0);
